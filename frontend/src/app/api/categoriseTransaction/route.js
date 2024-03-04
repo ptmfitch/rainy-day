01037a10dd7594301981cf169c769f351d3fc97c
James Osgood <james.osgood@gmail.com>
2024-03-04 20:32:43 +0000

    Plumb in getting txn from Mongo

--- frontend/src/app/api/categoriseTransaction/route.js
+++ frontend/src/app/api/categoriseTransaction/route.js
@@ -8,23 +8,25 @@ import { MongoDBAtlasVectorSearch } from '@langchain/mongodb';
 import { createStuffDocumentsChain } from 'langchain/chains/combine_documents';
 
 export async function GET(req, res) {
+  const searchParams = req.nextUrl.searchParams
+  const txn_id = searchParams.get('txn_id')
+
   const embeddings = new OpenAIEmbeddings({
     azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
     azureOpenAIApiVersion: process.env.AZURE_OPENAI_API_VERSION,
     azureOpenAIApiInstanceName: process.env.AZURE_OPENAI_API_INSTANCE_NAME,
-    azureOpenAIApiDeploymentName:
-      process.env.AZURE_OPENAI_EMBEDDINGS_DEPLOYMENT_NAME,
+    azureOpenAIApiDeploymentName: process.env.AZURE_OPENAI_EMBEDDINGS_DEPLOYMENT_NAME,
   });
 
   // Connection URL
   const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
   const client = await MongoClient.connect(MONGODB_URI);
-  const collection = client
+  const rag_collection = client
     .db('rainyday')
     .collection('category_by_description');
 
   const vectorStore = new MongoDBAtlasVectorSearch(embeddings, {
-    collection,
+    collection: rag_collection,
     indexName: 'vector_index', // The name of the Atlas search index. Defaults to "default"
     textKey: 'text_key', // The name of the collection field containing the raw content. Defaults to "text"
     embeddingKey: 'embedding', // The name of the collection field containing the embedded text. Defaults to "embedding"
@@ -32,7 +34,6 @@ export async function GET(req, res) {
 
   // Retrieve and generate using the relevant snippets of the blog.
   const retriever = vectorStore.asRetriever();
-  //const prompt = (await pull) < ChatPromptTemplate > 'rlm/rag-prompt';
 
   // If a template is passed in, the input variables are inferred automatically from the template.
   const prompt = PromptTemplate.fromTemplate(
@@ -42,7 +43,6 @@ export async function GET(req, res) {
   {context}`
   );
 
-  console.log(prompt);
   const llm = new ChatOpenAI({
     azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
     azureOpenAIApiVersion: process.env.AZURE_OPENAI_API_VERSION,
@@ -57,7 +57,13 @@ export async function GET(req, res) {
     outputParser: new StringOutputParser(),
   });
 
-  const description = 'Starbucks';
+  const txn_collection = client
+    .db('rainyday')
+    .collection('transactions');
+
+  const txn = await txn_collection.findOne({'_id' : txn_id});
+
+  const description = txn['description'];
   const question = `What is the category for "${description}". If you dont know, give the answer <UNKNOWN> and include the input. Output a raw json document with fields for description, category and reasoning`;
 
   const retrievedDocs = await retriever.getRelevantDocuments(description);
@@ -67,6 +73,7 @@ export async function GET(req, res) {
     context: retrievedDocs,
   });
 
+  client.close();
   console.log(result);
 
   return NextResponse.json(result);
