a7836ceebf3b4d24497c150fde0baf5d072c1262
Peter Fitch <ptmfitch@users.noreply.github.com>
2024-03-04 11:09:36 +0000

    placeholder navigation

--- src/app/components/LeafyAppShell.js
+++ src/app/components/LeafyAppShell.js
@@ -7,6 +7,14 @@ import { usePathname } from "next/navigation";
 import classes from './LeafyAppShell.module.css';
 import SearchInput from './SearchInput';
 
+const pages = [
+  {name: "Home", href: "/"},
+  {name: "Categories", href: "/categories"},
+  {name: "Transactions", href: "/transactions"},
+  {name: "Weather", href: "/weather"},
+  {name: "Pictures", href: "/pictures"}
+]
+
 export default function LeafyAppShell({children}) {
   const pathname = usePathname();
   const [opened, { toggle }] = useDisclosure();
@@ -41,14 +49,17 @@ export default function LeafyAppShell({children}) {
         bg="gray.0"
       >
         <h1 className={classes.h1}>NAVIGATION</h1>
-        <NavLink
-          href="/"
-          label="Home"
-          active={pathname === '/'}
-          variant="filled"
-          c={pathname === '/' ? 'green.7' : 'gray.6'}
-          bg={pathname === '/' ? 'green.1' : 'gray.0'}
-        />
+        {pages.map((page, index) => (
+          <NavLink
+            key={index}
+            href={page.href}
+            label={page.name}
+            active={pathname === page.href}
+            variant="filled"
+            c={pathname === page.href ? 'green.7' : 'gray.6'}
+            bg={pathname === page.href ? 'green.1' : 'gray.0'}
+          />
+        ))}
       </AppShell.Navbar>
 
       <AppShell.Main>{children}</AppShell.Main>
