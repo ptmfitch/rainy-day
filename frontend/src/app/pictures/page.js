edc9bd5d220e024c819297a3486732c6107c3a18
Peter Fitch <ptmfitch@users.noreply.github.com>
2024-03-04 21:33:44 +0000

    photo upload ui

--- frontend/src/app/pictures/page.js
+++ frontend/src/app/pictures/page.js
@@ -1,3 +1,18 @@
+'use client';
+
+import { FileInput, Stack } from "@mantine/core"
+import { useState } from "react"
+
 export default function Pictures() {
-  return (<></>)
+  const [image, setImage] = useState(null);
+
+  return (<Stack p="xl">
+    <FileInput 
+      value={image} onChange={setImage}
+      w="100%"
+      accept="image/*"
+      label="Upload an image"
+      placeholder="No image selected"
+    />
+  </Stack>)
 }
