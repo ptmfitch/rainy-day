475bfb3003990d4b01cd4191d25f4ec4483290b2
Peter Fitch <ptmfitch@users.noreply.github.com>
2024-03-04 21:29:42 +0000

    stories modal

--- frontend/src/app/components/Card.js
+++ frontend/src/app/components/Card.js
@@ -1,15 +1,17 @@
-import { Paper, Stack, Title, Group, ActionIcon, Loader, Center, Flex, Overlay } from "@mantine/core";
+import { Paper, Stack, Title, Group, ActionIcon, Loader, Text, Modal, Divider } from "@mantine/core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LeafyButton from "./LeafyButton";
 import LeafyBadge from "./LeafyBadge";
 import useSWR from 'swr';
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { useDisclosure } from "@mantine/hooks";
 
 import classes from './MantineCarousel.module.css';
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
-export function Card({ embla, title, category}) {
+export function Card({ embla, title, category, insight, detail}) {
+  const [opened, { open, close }] = useDisclosure(false);
   const handleNext = () => embla?.scrollNext();
   const handlePrev = () => embla?.scrollPrev();
   const { data, isLoading } = useSWR('/api/categoryImageLookup?query=' + category, fetcher, {revalidateOnFocus: false})
@@ -33,30 +35,33 @@ export function Card({ embla, title, category}) {
           
           <Paper w="100%" justify="center" align="center" p="sm" bg="green.1" radius="lg">
             <Title 
-            radius="md"
+              radius="md"
               order={3} 
               c="black"
             >
               {title}
-          </Title>
-          
-          
+            </Title>
           </Paper>
           <LeafyBadge text={category}/>
-          
           
         </Stack>
         <Group w="100%" justify="space-between">
           <ActionIcon onClick={handlePrev} color="green.7">
             <FontAwesomeIcon icon={faArrowLeft} />
           </ActionIcon>
-          {/*todo modal appear */}
-          <LeafyButton variant="primary">Read more</LeafyButton>
+          <LeafyButton variant="primary" onClick={open}>Read more</LeafyButton>
           <ActionIcon onClick={handleNext} color="green.7">
             <FontAwesomeIcon icon={faArrowRight} />
           </ActionIcon>
         </Group>
       </Stack>
+
+      <Modal opened={opened} onClose={close} title={title} centered>
+        <Stack>
+          <Text>{insight}</Text>
+          <Text>{detail}</Text>
+        </Stack>
+      </Modal>
       
     </Paper>
   );
