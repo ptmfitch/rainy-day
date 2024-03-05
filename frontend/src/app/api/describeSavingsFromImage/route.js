4ed5e5cee61704bdc7dae3ea3121978fe8d73cf1
Ralph Johnson <ralphjohnson@mongodb.com>
2024-03-05 11:43:54 +0000

    2nd cut of Image recognition (working)

--- frontend/src/app/api/describeSavingsFromImage/route.js
+++ frontend/src/app/api/describeSavingsFromImage/route.js
@@ -1,41 +1,16 @@
 import { NextResponse, NextRequest } from 'next/server';
 import { writeFile } from 'fs/promises';
 import { join } from 'path';
-import { ChatOpenAI } from '@langchain/openai';
 import AWS from 'aws-sdk';
-
+import fs from 'fs';  
+import axios from 'axios'; 
 // -------
 
-import { MongoClient } from 'mongodb';
-
-// Connection URL
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
-const client = await MongoClient.connect(MONGODB_URI);
-const coll = client.db('rainyday').collection('images');
-
-//  -------
-console.log('azure key', process.env.AZURE_OPENAI_API_KEY_4_0_VISION);
-const model = new ChatOpenAI({
-  temperature: 0.9,
-  azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY_4_0_VISION,
-  azureOpenAIApiVersion: process.env.AZURE_OPENAI_API_VERSION_4_0_VISION,
-  azureOpenAIApiInstanceName:
-    process.env.AZURE_OPENAI_API_INSTANCE_NAME_4_0_VISION,
-  azureOpenAIApiDeploymentName:
-    process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME_4_0_VISION,
-  maxTokens: 2500,
-});
-
-console.log('model', model);
-
 export async function POST(req, res) {
-  console.log('got here');
   try {
     // Access uploaded file
     const file = await req.formData();
-    console.log(file);
     const imageFile = file.get('file');
-    console.log(imageFile);
 
     const bytes = await imageFile.arrayBuffer();
     const buffer = Buffer.from(bytes);
@@ -56,32 +31,68 @@ export async function POST(req, res) {
 
     const uniqueFilename = `${Date.now()}-${imageFile.name}`;
 
-    // upload to Mongo
-    await coll.insertOne({ name: uniqueFilename, file: buffer });
-    console.log('Inserted into MongoDB');
-
-    await client.close();
-
     const filename = join('/tmp/', uniqueFilename);
     console.log(filename);
 
     await writeFile(filename, buffer);
     console.log(`Saved to ${filename}`);
+    const IMAGE_PATH = filename;  
 
-    /*const loader = new UnstructuredLoader(filename);
-        console.log("loader: ", loader);
-        const docs = await loader.load();
-        console.log(docs[0].pageContent);
-        const response_raw = docs[0].pageContent;
-        */
+    const encoded_image = Buffer.from(fs.readFileSync(IMAGE_PATH)).toString('base64');  
 
-    const prompt = `Describe the contents of the image that isn't attached (use your imaginitive powers). \n`;
-    console.log('prompt:', prompt);
-    const result = await model.invoke(prompt); //,response_raw);
-    console.log('result:', result);
+    const headers = {  
+      "Content-Type": "application/json",  
+      "api-key": process.env.AZURE_OPENAI_API_KEY_4_0_VISION,  
+    };  
+      
+    // Payload for the request  
+    const payload = {  
+      "messages": [  
+        {  
+          "role": "system",  
+          "content": [  
+            {  
+              "type": "text",  
+              "text": "You are an AI assistant that helps people find information."  
+            }  
+          ]  
+        },  
+        {  
+          "role": "user",  
+          "content": [  
+            {  
+              "type": "image_url",  
+              "image_url": {  
+                "url": `data:image/jpeg;base64,${encoded_image}`  
+              }  
+            },  
+            {  
+              "type": "text",  
+              "text": 'Based on the image, describe 3 ways I could save money in the following form [{"title":"Headline","insight":"Humorous insight","detail":"Detailed output including values","category":"OneWordCategory"}].  Don\'t make any of the one word categories generic.'
+            }  
+          ]  
+        }  
+      ],  
+      "temperature": 0.7,  
+      "top_p": 0.95,  
+      "max_tokens": 800  
+    };  
+      
+    const GPT4V_ENDPOINT = `https://${process.env.AZURE_OPENAI_API_INSTANCE_NAME_4_0_VISION}.openai.azure.com/openai/deployments/${process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME_4_0_VISION}/chat/completions?api-version=${process.env.AZURE_OPENAI_API_VERSION_4_0_VISION}`;  
+      
+    // Send request  
+    axios.post(GPT4V_ENDPOINT, payload, { headers: headers })  
+      .then(response => {  
+        console.log((response.data.choices[0].message.content));  
+        return NextResponse.json(response.data.choices[0].message.content);
+      })  
+      .catch(error => {  
+        console.error(`Failed to make the request. Error: ${error}`);  
+      });  
+
 
     // Respond with success message
-    return NextResponse.json({ message: 'File uploaded successfully' });
+    return NextResponse.json({ message: 'File uploaded failed successfully' });
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error: 'Internal server error' });
