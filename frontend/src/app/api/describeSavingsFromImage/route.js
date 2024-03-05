43d97979e31e9458d06de15327b1b3975ebf3130
Carlos Castro <carlos.castro@mongodb.com>
2024-03-05 13:10:12 +0000

    Add Azure OpenAI API keys and configurations*** ***Update next.config.mjs to include new image domains*** ***Refactor uploadFile function to include setting image URL*** ***Update Pictures component to display uploaded image

--- frontend/src/app/api/describeSavingsFromImage/route.js
+++ frontend/src/app/api/describeSavingsFromImage/route.js
@@ -2,8 +2,8 @@ import { NextResponse, NextRequest } from 'next/server';
 import { writeFile } from 'fs/promises';
 import { join } from 'path';
 import AWS from 'aws-sdk';
-import fs from 'fs';  
-import axios from 'axios'; 
+import fs from 'fs';
+import axios from 'axios';
 // -------
 
 export async function POST(req, res) {
@@ -25,8 +25,9 @@ export async function POST(req, res) {
       Bucket: process.env.BUCKET_NAME,
       Key: imageFile.name,
       Body: buffer,
+      acl: 'public-read',
     };
-    await s3.upload(params).promise();
+    const s3File = await s3.upload(params).promise();
     console.log('Uploaded to S3');
 
     const uniqueFilename = `${Date.now()}-${imageFile.name}`;
@@ -36,63 +37,73 @@ export async function POST(req, res) {
 
     await writeFile(filename, buffer);
     console.log(`Saved to ${filename}`);
-    const IMAGE_PATH = filename;  
+    const IMAGE_PATH = filename;
 
-    const encoded_image = Buffer.from(fs.readFileSync(IMAGE_PATH)).toString('base64');  
+    const encoded_image = Buffer.from(fs.readFileSync(IMAGE_PATH)).toString(
+      'base64'
+    );
 
-    const headers = {  
-      "Content-Type": "application/json",  
-      "api-key": process.env.AZURE_OPENAI_API_KEY_4_0_VISION,  
-    };  
-      
-    // Payload for the request  
-    const payload = {  
-      "messages": [  
-        {  
-          "role": "system",  
-          "content": [  
-            {  
-              "type": "text",  
-              "text": "You are an AI assistant that helps people find information."  
-            }  
-          ]  
-        },  
-        {  
-          "role": "user",  
-          "content": [  
-            {  
-              "type": "image_url",  
-              "image_url": {  
-                "url": `data:image/jpeg;base64,${encoded_image}`  
-              }  
-            },  
-            {  
-              "type": "text",  
-              "text": 'Based on the image, describe 3 ways I could save money in the following form [{"title":"Headline","insight":"Humorous insight","detail":"Detailed output including values","category":"OneWordCategory"}].  Don\'t make any of the one word categories generic.'
-            }  
-          ]  
-        }  
-      ],  
-      "temperature": 0.7,  
-      "top_p": 0.95,  
-      "max_tokens": 800  
-    };  
-      
-    const GPT4V_ENDPOINT = `https://${process.env.AZURE_OPENAI_API_INSTANCE_NAME_4_0_VISION}.openai.azure.com/openai/deployments/${process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME_4_0_VISION}/chat/completions?api-version=${process.env.AZURE_OPENAI_API_VERSION_4_0_VISION}`;  
-      
-    // Send request  
-    const res = await axios.post(GPT4V_ENDPOINT, payload, { headers: headers })  
-      .then(response => {  
-        console.log((response.data.choices[0].message.content));  
+    const headers = {
+      'Content-Type': 'application/json',
+      'api-key': process.env.AZURE_OPENAI_API_KEY_4_0_VISION,
+    };
+
+    // Payload for the request
+    const payload = {
+      messages: [
+        {
+          role: 'system',
+          content: [
+            {
+              type: 'text',
+              text: 'You are an AI assistant that helps people find information.',
+            },
+          ],
+        },
+        {
+          role: 'user',
+          content: [
+            {
+              type: 'image_url',
+              image_url: {
+                url: `data:image/jpeg;base64,${encoded_image}`,
+              },
+            },
+            {
+              type: 'text',
+              text: 'Based on the image, describe 3 ways I could save money in the following form [{"title":"Headline","insight":"Humorous insight","detail":"Detailed output including values","category":"OneWordCategory"}].  Don\'t make any of the one word categories generic.',
+            },
+          ],
+        },
+      ],
+      temperature: 0.7,
+      top_p: 0.95,
+      max_tokens: 800,
+    };
+
+    const GPT4V_ENDPOINT = `https://${process.env.AZURE_OPENAI_API_INSTANCE_NAME_4_0_VISION}.openai.azure.com/openai/deployments/${process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME_4_0_VISION}/chat/completions?api-version=${process.env.AZURE_OPENAI_API_VERSION_4_0_VISION}`;
+
+    // Send request
+    const res = await axios
+      .post(GPT4V_ENDPOINT, payload, { headers: headers })
+      .then((response) => {
+        console.log(response.data.choices[0].message.content);
         return response.data.choices[0].message.content;
-      })  
-      .catch(error => {  
-        console.error(`Failed to make the request. Error: ${error}`);  
-      });  
+      })
+      .catch((error) => {
+        console.error(`Failed to make the request. Error: ${error}`);
+      });
 
+    // https://rainyday-s3-bucket.s3.eu-west-2.amazonaws.com/Screenshot+2024-03-04+at+13.34.57.png
+
+    const result = JSON.parse(res);
+
+    result.push({
+      url: s3File.Location,
+    });
 
     // Respond with success message
-    return NextResponse.json(JSON.parse(res));
+    return NextResponse.json(result);
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error: 'Internal server error' });
