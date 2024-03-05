56c49b97dce2d9c0977bca2f26d9652049143a34
Peter Fitch <ptmfitch@users.noreply.github.com>
2024-03-05 01:29:53 +0000

    categorised transaction totals page

--- frontend/src/app/transactions/page.js
+++ frontend/src/app/transactions/page.js
@@ -1,3 +1,32 @@
+'use client';
+
+import useSWR from "swr"
+import { Center, Loader, Stack } from "@mantine/core";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
+function replaceUnderscoresWithSpaces(s) {
+  return s.replace(/_/g, ' ')
+}
+
+function capitaliseFirstLetter(s) {
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+function cleanCategoryName(s) {
+  if (s) {
+    return capitaliseFirstLetter(replaceUnderscoresWithSpaces(s))
+  } else {
+    return "Uncategorised"
+  }
+}
+
 export default function Transactions() {
-  return (<></>)
+  const { data: transactions, isLoading: transactionsLoading } = useSWR('/api/totalPerCategory', fetcher)
+  return (<>
+    {transactionsLoading && <Center mt="md"><Loader /></Center>}
+    {transactions && <Stack p="md">{transactions.sort((a, b) => a.total - b.total).reverse().map((transaction) => {
+      return (<div key={transaction._id}>{cleanCategoryName(transaction._id)} - £{transaction.total}</div>)
+    })}</Stack>}
+  </>)
 }
